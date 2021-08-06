417bf31b8301fe49a7cd69f7d689f8782de42253
handapeng <865804820@qq.com>
2021-08-06 15:21:22 +0800

    dubbo

---
+++ src/main/java/com/example/dubbomvn/DubbomvnApplication.java
@@ -0,0 +1,13 @@
+package com.example.dubbomvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubbomvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubbomvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dubbomvn/DubbomvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dubbomvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubbomvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
